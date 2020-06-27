ea8e8f8f6fcd357efb61c734294eeae5a0bb507e
tiago-silva-analista <tiago.silva.analista@gmail.com>
2020-06-27 14:40:38 -0300

    adjusts

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -4,6 +4,26 @@ import {Request, Response} from 'express';
 
 class PointsController{
 
+    async index(request:Request, response:Response){
+        const {city, uf, items} = request.query;
+
+        const parsedItems = String(items)
+                            .split(',')
+                            .map(item => Number(item.trim()));
+         
+        const points = await knex('points')
+                        .join('point_items','points.id', '=','point_items.point_id')
+                        .whereIn('point_items.item_id',parsedItems)
+                        .where('city', String(city))
+                        .where('uf',String(uf))
+                        .distinct()
+                        .select('points.*');
+                            
+        //console.log(city, uf,items);
+
+        return response.json(points);
+    }
+
     async show(request:Request, response: Response){
         const {id} = request.params;
 
@@ -41,7 +61,7 @@ class PointsController{
         const trx = await knex.transaction();
 
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name,
             email,
             whatsapp,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,5 +12,6 @@ routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
 routes.get('/points/:id', pointsController.show);
+routes.get('/points/', pointsController.index);
 
 export default routes;
