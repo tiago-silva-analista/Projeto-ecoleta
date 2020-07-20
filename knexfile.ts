ec80827f14430295db8e4691a2abc58b0f846811
tiago-silva-analista <tiago.silva.analista@gmail.com>
2020-07-20 18:35:43 -0300

    connection adjust

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 
+
+
 module.exports = {
     client: 'sqlite3',
-    connection: {
-        filename: path.resolve(__dirname,'src','database','database.sqlite'),
+    connection: process.env.DATABASE_URL
     },
     migrations: {
         directory: path.resolve(__dirname, 'src','database','migrations')
