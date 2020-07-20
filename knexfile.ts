8c682d3d74635628782574d43320414f40370a64
tiago-silva-analista <tiago.silva.analista@gmail.com>
2020-07-20 19:08:19 -0300

    connection adjust

--- knexfile.ts
+++ knexfile.ts
@@ -3,13 +3,13 @@ import path from 'path';
 
 
 module.exports = {
-    client: 'sqlite3',
-    connection: {
-        host: 'ec2-18-214-119-135.compute-1.amazonaws.com',
-        database: 'd5590fgf39adp7',
-        user: 'louxpxzvjhkmkx',
-        password: '0b4dcc588a32873f125d19e8fc3d17e983a6b808e96f51542d263aaaf4895556'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+        // host: 'ec2-18-214-119-135.compute-1.amazonaws.com',
+        // database: 'd5590fgf39adp7',
+        // user: 'louxpxzvjhkmkx',
+        // password: '0b4dcc588a32873f125d19e8fc3d17e983a6b808e96f51542d263aaaf4895556'
+    //},
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
