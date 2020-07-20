00dd97b0dba3e2cad219ed3bf15128e7d62ebe55
tiago-silva-analista <tiago.silva.analista@gmail.com>
2020-07-20 18:50:17 -0300

    connection adjust

--- knexfile.ts
+++ knexfile.ts
@@ -4,13 +4,17 @@ import path from 'path';
 
 module.exports = {
     client: 'sqlite3',
-    connection: process.env.DATABASE_URL
+    connection: {
+        host: 'ec2-18-214-119-135.compute-1.amazonaws.com',
+        database: 'd5590fgf39adp7',
+        user: 'louxpxzvjhkmkx',
+        password: '0b4dcc588a32873f125d19e8fc3d17e983a6b808e96f51542d263aaaf4895556'
     },
     migrations: {
-        directory: path.resolve(__dirname, 'src','database','migrations')
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
     seeds: {
-        directory: path.resolve(__dirname, 'src','database','seeds')
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds')
     },
     useNullAsDefault: true,
 }
